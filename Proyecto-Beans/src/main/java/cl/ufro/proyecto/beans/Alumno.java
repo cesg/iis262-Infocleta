50a889181ecb950e2b41738526f55e9d870f49c5
cesg <cesg.dev@gmail.com>
2013-05-27 15:44:29 -0400

    Proyecto-Beans: Alumno.

---
+++ Proyecto-Beans/src/main/java/cl/ufro/proyecto/beans/Alumno.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cl.ufro.proyecto.beans;
+
+/**
+ *
+ * @author kristian
+ */
+public class Alumno {
+    private String nombre;
+    private String matricula;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public Alumno(String nombre, String matricula, String apellidoMaterno) {
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }    
+}
