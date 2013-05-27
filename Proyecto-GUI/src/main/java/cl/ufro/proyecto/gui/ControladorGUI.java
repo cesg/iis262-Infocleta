4deaef28fabf9540b171559b7ba9f58814a4c767
cesg <cesg.dev@gmail.com>
2013-05-27 00:46:36 -0400

    javadoc

--- Proyecto-GUI/src/main/java/cl/ufro/proyecto/gui/ControladorGUI.java
+++ Proyecto-GUI/src/main/java/cl/ufro/proyecto/gui/ControladorGUI.java
@@ -5,8 +5,10 @@
 package cl.ufro.proyecto.gui;
 
 /**
- * Es la comunicación con la GUI
- * @author 
+ * Es la comunicación con la GUI Se debe ir agregabdo cada método necesario en
+ * la GUI
+ *
+ * @author cesg
  */
 public interface ControladorGUI {
     
