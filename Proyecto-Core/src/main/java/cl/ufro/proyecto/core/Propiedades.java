8cc137896facb3f967ce0c21c2aeb52c1bbc7caf
cesg <cesg.dev@gmail.com>
2013-05-27 15:59:53 -0400

    Core: Mejoras en Propiedades.

--- Proyecto-Core/src/main/java/cl/ufro/proyecto/core/Propiedades.java
+++ Proyecto-Core/src/main/java/cl/ufro/proyecto/core/Propiedades.java
@@ -6,7 +6,6 @@ package cl.ufro.proyecto.core;
 
 import cl.ufro.proyecto.core.Enums.LAF;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,24 +17,30 @@ import org.slf4j.LoggerFactory;
 public class Propiedades extends ArchivoPropiedades {
 
     private Properties prop;
-    private URL fileProp;
+//    private URL fileProp;
     private Logger LOG = LoggerFactory.getLogger(Propiedades.class);
 
     public Propiedades() {
         this.prop = new Properties();
-        fileProp = getClass().getClassLoader().getResource(NOMBRE);
+//        fileProp = getClass().getClassLoader().getResource(NOMBRE);
         try {
             if (direccion != null) {
                 this.prop.load(direccion.openStream());
             }
         } catch (IOException ex) {
-            LOG.error("Error", ex);
+            LOG.error("Imposible leer el archivo de opciones.", ex);
         }
     }
 
     public LAF getLookAndFeel() {
         String property = prop.getProperty(KEY_LAF);
-//TODO: switch
-        return LAF.JTATOO;
+        LAF laf = LAF.SYSTEM;
+        if (property == null) {
+            return laf;
+        }
+        if (property.equalsIgnoreCase(lafValoresSoportados.JTATTOO.getValue())) {
+            laf = LAF.JTATOO;
+        }
+        return laf;
     }
 }
