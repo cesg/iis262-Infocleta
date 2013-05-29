3849e426a544d9e59057a64ab22ea789a447d1cc
cesg <cesg.dev@gmail.com>
2013-05-29 12:40:42 -0400

    Beans: Utilidades.

---
+++ Proyecto-Beans/src/main/java/cl/ufro/proyecto/beans/utils/Utilidades.java
@@ -0,0 +1,18 @@
+package cl.ufro.proyecto.beans.utils;
+
+/**
+ * Contiene métodos utilitarios.
+ * @author kristian
+ *
+ */
+public final class Utilidades {
+    /**
+     * <p>Verficia si un Objeto es nulo.</p>
+     * boolean esNulo(final Object o)
+     * @param o
+     * @return
+     */
+    public static boolean esNulo(final Object o){
+        return o==null;
+    }
+}
