57cd864550bd52ed13e9624509dc4b9b6b32bff4
cesg <cesg.dev@gmail.com>
2013-05-28 22:29:17 -0400

    Beans: lista de alumnos.

---
+++ Proyecto-Beans/src/main/java/cl/ufro/proyecto/beans/ListaAlumnosImp.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package cl.ufro.proyecto.beans;
+
+/**
+ * @author kristian
+ *
+ */
+public class ListaAlumnosImp implements ListaAlumnos {
+
+
+    public void insertar(Alumno a) {
+	// TODO Auto-generated method stub
+
+    }
+
+
+    public boolean eliminar(Alumno a) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+
+    public Alumno obtener(String matricula) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+
+    public boolean contiene(Alumno a) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+
+    public Alumno ultimo() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+
+    public Alumno primero() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+
+    public boolean estaVacia() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    public int largo() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+
+    public void ordenarPorNombre() {
+	// TODO Auto-generated method stub
+	
+    }
+
+
+    public void ordenarPorApellidoMaterno() {
+	// TODO Auto-generated method stub
+	
+    }
+
+
+    public void ordenarPorApellidoPaterno() {
+	// TODO Auto-generated method stub
+	
+    }
+
+}
