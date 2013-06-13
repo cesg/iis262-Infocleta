cb9c9092bda37aa191950a46222e784bb781475a
cesg <cesg.dev@gmail.com>
2013-06-13 15:06:25 -0400

    Core: métodos del controlador.

--- Proyecto-Core/src/main/java/cl/ufro/proyecto/core/SimpleControladorGUI.java
+++ Proyecto-Core/src/main/java/cl/ufro/proyecto/core/SimpleControladorGUI.java
@@ -4,6 +4,8 @@
  */
 package cl.ufro.proyecto.core;
 
+import cl.ufro.proyecto.beans.Alumno;
+import cl.ufro.proyecto.beans.ListaAlumnos;
 import cl.ufro.proyecto.gui.ControladorGUI;
 
 /**
@@ -11,5 +13,23 @@ import cl.ufro.proyecto.gui.ControladorGUI;
  * @author kristian
  */
 public class SimpleControladorGUI implements ControladorGUI{
+
+    @Override
+    public void agregarAlumno(Alumno a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Alumno buscarAlumno(Alumno a) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListaAlumnos todosAlumnos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
