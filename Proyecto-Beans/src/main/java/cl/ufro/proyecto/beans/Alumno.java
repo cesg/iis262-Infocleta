3a78071f9e04175c3eca47a0a2fc7e02dfcb52ad
cesg <cesg.dev@gmail.com>
2013-06-14 21:09:43 -0400

    Beans: Alumno mejoras.

--- Proyecto-Beans/src/main/java/cl/ufro/proyecto/beans/Alumno.java
+++ Proyecto-Beans/src/main/java/cl/ufro/proyecto/beans/Alumno.java
@@ -2,7 +2,7 @@ package cl.ufro.proyecto.beans;
 
 /**
  * <p>
- * Representa un alumno. Implementa  <code>java.lang.Comparable </code> para
+ * Representa un alumno. Implementa <code>java.lang.Comparable </code> para
  * comparar un alumno de otro segun su matricula.
  * </p>
  * 
@@ -14,83 +14,94 @@ public class Alumno implements Comparable<Alumno> {
     private String apellidoPaterno;
     private String apellidoMaterno;
 
+    /**
+     * Nuevo alumno solo con su nombre y matricula.
+     * 
+     * @param nombre
+     * @param matricula
+     */
+    public Alumno(String nombre, String matricula) {
+	this.nombre = nombre;
+	this.matricula = matricula;
+    }
+
     public Alumno(String nombre, String matricula, String apellidoMaterno,
-            String apellidoPaterno) {
-        this.nombre = nombre;
-        this.matricula = matricula;
-        this.apellidoMaterno = apellidoMaterno;
-        this.apellidoPaterno = apellidoPaterno;
+	    String apellidoPaterno) {
+	this.nombre = nombre;
+	this.matricula = matricula;
+	this.apellidoMaterno = apellidoMaterno;
+	this.apellidoPaterno = apellidoPaterno;
     }
 
     public String getNombre() {
-        return nombre;
+	return nombre;
     }
 
     public String getMatricula() {
-        return matricula;
+	return matricula;
     }
 
     public String getApellidoPaterno() {
-        return apellidoPaterno;
+	return apellidoPaterno;
     }
 
     public String getApellidoMaterno() {
-        return apellidoMaterno;
+	return apellidoMaterno;
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+	this.nombre = nombre;
     }
 
     public void setMatricula(String matricula) {
-        this.matricula = matricula;
+	this.matricula = matricula;
     }
 
     public void setApellidoPaterno(String apellidoPaterno) {
-        this.apellidoPaterno = apellidoPaterno;
+	this.apellidoPaterno = apellidoPaterno;
     }
 
     public void setApellidoMaterno(String apellidoMaterno) {
-        this.apellidoMaterno = apellidoMaterno;
+	this.apellidoMaterno = apellidoMaterno;
     }
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((matricula == null) ? 0 : matricula.hashCode());
-        return result;
+	final int prime = 31;
+	int result = 1;
+	result = prime * result
+		+ ((matricula == null) ? 0 : matricula.hashCode());
+	return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Alumno other = (Alumno) obj;
-        if (matricula == null) {
-            if (other.matricula != null) {
-                return false;
-            }
-        } else if (!matricula.equals(other.matricula)) {
-            return false;
-        }
-        return true;
+	if (this == obj) {
+	    return true;
+	}
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	Alumno other = (Alumno) obj;
+	if (matricula == null) {
+	    if (other.matricula != null) {
+		return false;
+	    }
+	} else if (!matricula.equals(other.matricula)) {
+	    return false;
+	}
+	return true;
     }
 
     public String toString() {
-        return this.nombre;
+	return this.nombre;
     }
 
     public int compareTo(Alumno a) {
-        // TODO Auto-generated method stub
-        return 0;
+	// int resultado = matricula.compareTo(a.getMatricula());
+	return matricula.compareTo(a.getMatricula());
     }
 }
