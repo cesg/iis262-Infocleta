1500f9d9abc21639758d0dd777e9665102f8dd62
cesg <cesg.dev@gmail.com>
2013-06-15 19:33:30 -0400

    Core: ListaEnlazada mejoras en al insertar.

--- Proyecto-Core/src/test/java/cl/ufro/proyecto/core/imp/linked/TestListaEnlazada.java
+++ Proyecto-Core/src/test/java/cl/ufro/proyecto/core/imp/linked/TestListaEnlazada.java
@@ -9,17 +9,23 @@ import cl.ufro.proyecto.beans.ListaAlumnos;
 
 public class TestListaEnlazada {
     ListaAlumnos listaAlumnos;
+
     @Test
     public void testInsertar() {
-	listaAlumnos = new ListaEnlazada();
-	Alumno a = new Alumno("Dummy", "12345678", null, null);
-//	Alumno a2 = new Alumno("Dummy2", "10345678", null, null);
-	listaAlumnos.insertar(a);
-//	listaAlumnos.insertar(a2);
-	for (Alumno alumno : listaAlumnos) {
-	    System.out.print(alumno);
-	    System.out.print(" - ");
-	}
+        ListaEnlazada listaAlumnos = new ListaEnlazada();
+        Alumno a = new Alumno("Dummy", "1");
+        Alumno a2 = new Alumno("Dummy2", "0");
+        Alumno a3 = new Alumno("Dummy3", "2");
+        Alumno a4 = new Alumno("Dummy2", "6");
+        listaAlumnos.insertar(a);
+        listaAlumnos.insertar(a2);
+        listaAlumnos.insertar(a3);
+        listaAlumnos.insertar(a4);
+        for (Alumno alumno : listaAlumnos) {
+            System.out.print(alumno);
+            System.out.print(" ");
+        }
+        System.out.println();
+        listaAlumnos.imprimeDesdeUlt();
     }
-
 }
